62dd74e7e6ec25d414fcbe2d79da25191672495c
vineet311 <vineetpandit003@gmail.com>
2025-03-20 20:49:08 +0530

    index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
-import React, { useState, useEffect } from "react";
+
+
+import React from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("/api/message")
-      .then((res) => res.text())
-      .then((data) => setMessage(data));
-  }, []);
-
-  return <h1>{message}</h1>;
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+    </div>
+  );
 }
 
 export default App;
