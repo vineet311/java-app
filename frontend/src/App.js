1ed6a7bff24aa6ecd0cbfbe0550b67b92cdea35c
vinet311 <vineetpandit003@gmail.com>
2025-03-21 21:59:18 +0530

    ReactApp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
-
 import React from "react";
+import "./styles.css"; // Import the CSS file
 
 function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
+    <div className="container">
+      <h1>Welcome to My React App 🚀</h1>
+      <p>This is a simple React app with improved UI.</p>
+      <button className="btn">Click Me!</button>
     </div>
   );
 }
