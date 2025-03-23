28bcea1720a3d232cdd8f13ec7255462769f7bb1
vinet311 <vineetpandit003@gmail.com>
2025-03-23 22:42:58 +0530

    update src

--- backend/src/MessageController.java
+++ backend/src/MessageController.java
@@ -1,9 +1,11 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController  // This is necessary!
 @RequestMapping("/api")
 public class MessageController {
